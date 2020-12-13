df6d9df82dd8d9ff7d7843aed8f369d0c7734879
ttzztztz <ttzztztz@outlook.com>
2020-12-13 16:36:03 +0800

    feat: cache

---
+++ lib/store.ts
@@ -0,0 +1,48 @@
+export interface IData {
+  expire: number;
+  data: Buffer;
+}
+
+let store: { [key: string]: IData | undefined } = {};
+
+export const cacheGet = (key: string): Buffer | undefined => {
+  const _data = store[key];
+  if (_data !== undefined) {
+    const timestamp = Date.now();
+    if (_data.expire < timestamp) {
+      delete store[key];
+      return undefined;
+    }
+
+    return _data.data;
+  } else {
+    return undefined;
+  }
+};
+
+export const cacheSet = (key: string, value: Buffer, expire: number) => {
+  store[key] = {
+    data: value,
+    expire,
+  };
+};
+
+export const cacheClean = () => {
+  const nextStore: { [key: string]: IData | undefined } = {};
+  const timestamp = Date.now();
+
+  Object.entries(store).forEach(([key, value]) => {
+    const { expire } = value;
+    if (timestamp <= expire) {
+      nextStore[key] = value;
+    }
+  });
+  store = nextStore;
+};
+
+let interval: null | NodeJS.Timeout = null;
+if (interval === null) {
+  interval = setInterval(() => {
+    cacheClean();
+  }, 60 * 60 * 1000);
+}
--- pages/api/image/[window]/[username].ts
+++ pages/api/image/[window]/[username].ts
@@ -1,12 +1,21 @@
 import chrome from "chrome-aws-lambda";
 import { NextApiRequest, NextApiResponse } from "next";
 import puppeteer from "puppeteer-core";
+import { cacheGet, cacheSet } from "../../../../lib/store";
 
 const ENDPOINT = `https://github-contributions-curve.ttzztztz.vercel.app/`;
 const NODE_ENV = process.env.NODE_ENV;
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { window, username } = req.query;
+  const key = `${window}-${username}`;
+  const _cache = cacheGet(key);
+
+  if (_cache) {
+    res.setHeader("Content-Type", "image/png");
+    res.send(_cache);
+    return;
+  }
 
   const browser = await puppeteer.launch(
     NODE_ENV === "development"
@@ -35,5 +44,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     "base64"
   );
   res.setHeader("Content-Type", "image/png");
+  cacheSet(key, canvasBuffer, Date.now() + 2 * 60 * 60 * 1000);
   res.send(canvasBuffer);
 };
