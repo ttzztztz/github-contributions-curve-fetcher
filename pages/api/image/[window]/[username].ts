83dc457423fc418ff65561dedb20e06d60d33d46
ttzztztz <ttzztztz@outlook.com>
2020-12-13 15:49:27 +0800

    feat: aws

--- pages/api/image/[window]/[username].ts
+++ pages/api/image/[window]/[username].ts
@@ -1,16 +1,21 @@
+import chrome from "chrome-aws-lambda";
 import { NextApiRequest, NextApiResponse } from "next";
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-core";
 
 const ENDPOINT = `https://github-contributions-curve.ttzztztz.vercel.app/`;
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { window, username } = req.query;
 
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  });
   const page = await browser.newPage();
   await page.goto(`${ENDPOINT}/image/${window}/${username}`);
   const binary = await page.screenshot({ encoding: "binary" });
-  await browser.close();
+  await page.close();
 
   res.write(binary);
 };
